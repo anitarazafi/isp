89abe479a4692250e1c8aa152df1fdb49275aeb9
anitarazafi <anitarazafindrakotovao@gmail.com>
2023-05-26 00:19:16 +0300

    Added some components at home

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
-    <h1>Welcome</h1>
+    <x-app.hero></x-app.hero>
+    <x-app.forum></x-app.forum>
+    <x-app.guide></x-app.guide>
+    <x-app.cta></x-app.cta>
+    <x-app.posts></x-app.posts>
 </x-app-layout>
---
+++ resources/views/components/app/guide.blade.php
@@ -0,0 +1,54 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h1 class="mb-5">Student Guides</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <img class="img-fluid" src="{{asset('img/image_1.png')}}" alt="" />
+                        <h5>WorldWide Tours</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <img class="img-fluid" src="{{asset('img/image_1.png')}}" alt="" />
+                        <h5>WorldWide Tours</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <img class="img-fluid" src="{{asset('img/image_1.png')}}" alt="" />
+                        <h5>WorldWide Tours</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row justify-content-center py-5">
+            <div class="col-auto">
+                <a href="" class="btn btn-primary rounded-pill py-2 px-4">See all</a>
+            </div>
+        </div>
+    </div>
+</div>
